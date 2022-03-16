94fb3885c7f212dab9166596090516a120751f9e
Olya Gavrilko <olyaganeva@gmail.com>
2022-03-16 17:28:53 +0300

    first commit

---
+++ DecoratorPattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DecoratorPattern
+//
+//  Created by Olya Ganeva on 16.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
